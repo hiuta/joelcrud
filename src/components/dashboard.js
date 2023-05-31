8b7ae942fc7ffd7837ce441c725cc66deacfa9d8
Paulino Joel Martinez Vasquez <joel98vasquez@gmail.com>
2023-05-30 18:03:12 -0600

    instalacion de bootstrap e inicio del proyecyo

---
+++ src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Bienvenido a tu dashboard personalizado.</p>
+      {/* Aquí puedes agregar componentes adicionales, como gráficos, tablas, etc. */}
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi página de inicio</h1>
+      <p>Esta es la página de inicio de mi proyecto de React.</p>
+    </div>
+  );
+};
+
+export default Home;
