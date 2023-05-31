0fb1ff66ad6764c77e73777dc43b7bd642a3260e
Paulino Joel Martinez Vasquez <joel98vasquez@gmail.com>
2023-05-30 19:13:13 -0600

    Creacion de las carpetas y estructuras.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Home from './components/home';
-import Dashboard from './components/dashboard';
-import Header from './components/header';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Mi Aplicación de React</h1>
-      <Home />
-      <Dashboard />
-      <Header />
 
     </div>
 
